fb156519c58093fff794b97ddd7b1e96dfd747d3
Diana <d_quinlan061006@yahoo.com>
2019-03-13 15:57:18 -0400

    working code!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,8 +4,8 @@
 // 3.User guesses onkeyup and save
 // 4.Set limit of guesses to 10
 // 5.Show what user guesses and store and compare? Update results
-// 6. Display Losses and Wins
 // 6. Resets after each round
+// 7. Display Losses and Wins
 
 // The array of choices-letters
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
@@ -13,47 +13,38 @@ var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
 // variables for guesses and computer choice with outcomes
 var userGuess =[];
 var computerChoice;
-var startGame = false;
 var wins = 0;
 var losses = 0;
 var guessesLeft = 10;
 
 // start: randon computer choice
-document.onkeyup = function(event) {
-    var userGuess = event.key.toLowerCase();
-    var computerGuess = letters[Math.floor(Math.random() * letters.length)];
-    console.log("User Guess:" + userGuess);
-}
-
-// Start set up
-    function setUp () {
-        startGame = true;
-        computerChoice = letters[Math.floor(Math.random() * letters.length)];
-        guessesLeft = 10;
-        userGuess = [];
+    var computerChoice = letters[Math.floor(Math.random() * letters.length)];
+    console.log(computerChoice);
 
 
 //User guesses and outcomes
-    document.onkeypress = function(event) { var guess = event.key; userGuess.push(guess);
-    }  //collecting user guess
+ document.onkeypress = function(event) { var guess = event.key.toLocaleLowerCase(); userGuess.push(guess); 
+     
+    console.log("User Guess:" + userGuess);
+    //collecting user guess
 
-if (userGuess === computerChoice ){
-    wins(); //if they pick the same letter it is a win
+if (guess === computerChoice ){
+    win(); //if they pick the same letter it is a win
 }
 else { 
     guessesLeft--;  //if not the same decrease the number of guesses left
 }
     if (guessesLeft == 0) {  //if they run out of guesses they lose
-        losses();
+        loss();
     }
 
 // functions for the outcomes
-    function wins() {
+    function win() {
         wins++;
         reset();
     }   // if win  add a win to the score then start the game over
 
-    function losses() {
+    function loss() {
         losses++;
         reset();
     }    //if lose  add a loss to the score then start the game over 
@@ -62,8 +53,7 @@ else {
 
 function reset() {
     computerChoice = letters[Math.floor(Math.random() * letters.length)];
-    console.log("User Guess:" + userGuess);
-    startGame = true;
+    console.log(computerChoice);
     guessesLeft = 10;
     userGuess = [];
 
@@ -78,4 +68,5 @@ document.getElementById("left").innerHTML = "Guesses Left: " + guessesLeft;
 document.getElementById("wins").innerHTML = "Wins: " + wins;
 
 document.getElementById("losses").innerHTML = "Losses: " + losses;
-    }
+
+}  
